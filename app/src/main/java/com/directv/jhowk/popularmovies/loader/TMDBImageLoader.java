07335d9526cf0d2cf4b8234208b58e05c7eafe92
Jason Howk <jhowk@directv.com>
2016-04-08 16:40:10 -0600

    Add image loader.

---
+++ app/src/main/java/com/directv/jhowk/popularmovies/loader/TMDBImageLoader.java
@@ -0,0 +1,55 @@
+package com.directv.jhowk.popularmovies.loader;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import com.directv.jhowk.popularmovies.BuildConfig;
+import com.directv.jhowk.popularmovies.model.PopularMovie;
+import com.directv.jhowk.popularmovies.service.TMDBService;
+
+import java.util.Collection;
+
+/**
+ * Created by Jason Howk.
+ */
+public class TMDBImageLoader extends AsyncTaskLoader {
+    private static final String LOG_TAG = TMDBImageLoader.class.getSimpleName();
+
+
+    public TMDBImageLoader(Context context) {
+        super(context);
+        Log.d(LOG_TAG, "TMDBImageLoader: Creating loader.");
+    }
+
+    @Override
+    public Collection<PopularMovie> loadInBackground() {
+        Log.d(LOG_TAG, "loadInBackground: Attempting to load the popular results.");
+        Collection<PopularMovie> result = null;
+
+        try {
+            TMDBService tmdbSservice = TMDBService.get(getContext().getApplicationContext(), BuildConfig.TMDB_API_KEY);
+            result = tmdbSservice.getMoviesMostPopular();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        super.onStartLoading();
+        Log.d(LOG_TAG, "onStartLoading: On start loading...");
+        // TODO: display a spinner or something?
+//        if (takeContentChanged()) {
+        forceLoad();
+//        }
+    }
+
+    @Override
+    public void deliverResult(Object data) {
+        super.deliverResult(data);
+        Log.d(LOG_TAG, "deliverResult: Delivering results...");
+    }
+}
