04f215e56eae224a633ed31c47735d10b39ba644
Jason Howk <jhowk@directv.com>
2016-04-13 23:07:15 -0600

    Add backdrop paths.

--- app/src/main/java/com/directv/jhowk/popularmovies/service/TMDBService.java
+++ app/src/main/java/com/directv/jhowk/popularmovies/service/TMDBService.java
@@ -28,6 +28,7 @@ public class TMDBService {
     // API Constants.
     private static final String BASE_URL = "http://api.themoviedb.org/3"; // Will be replaced with call to configuration.
     private static final String BASE_IMAGE_URL = "http://image.tmdb.org/t/p/w500"; // Will be replaced with call to configuration.
+    private static final String BASE_BACKDROP_URL = "http://image.tmdb.org/t/p/w780"; // Will be replaced with call to configuration.
     private static final String API_KEY = "api_key";
     private static final String CONFIGURATION_URL = "configuration";
     private static final String POPULAR_URL = "movie/popular";
@@ -96,6 +97,10 @@ public class TMDBService {
 
     public static String getImagesBaseURL() {
         return BASE_IMAGE_URL;
+    }
+
+    public static String getBackdropBaseURL() {
+       return BASE_BACKDROP_URL;
     }
 
     ///////////////////////////////////////////////////////////////////////////
